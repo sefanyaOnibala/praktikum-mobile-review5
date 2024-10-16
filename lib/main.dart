556085de0044f5795a31e76e454ea9fca1c8c506
sefanyaOnibala <lomeyonibala@gmail.com>
2024-10-16 10:55:12 +0800

    Review5

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:pertemuan_5/pages/introduction_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nama Kalian',
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.light,
+        textTheme: const TextTheme(
+          headlineLarge: TextStyle(
+              fontSize: 48,
+              fontWeight: FontWeight.w600,
+              fontStyle: FontStyle.italic,
+              fontFamily: 'Serif'),
+          bodyLarge: TextStyle(color: Colors.black87),
+          bodyMedium: TextStyle(color: Colors.black54),
+          bodySmall: TextStyle(color: Colors.black45),
+        ),
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        textTheme: const TextTheme(
+          headlineLarge: TextStyle(
+              fontSize: 48,
+              fontWeight: FontWeight.w600,
+              fontStyle: FontStyle.italic,
+              fontFamily: 'Serif'),
+          bodyLarge: TextStyle(color: Colors.white70),
+          bodyMedium: TextStyle(color: Colors.white60),
+          bodySmall: TextStyle(color: Colors.white54),
+        ),
+      ),
+      themeMode: ThemeMode.system,
+      home: const IntroductionPage(),
+    );
+  }
+}
